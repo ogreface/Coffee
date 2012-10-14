66ccde5d3666bc10f2d7e86d082c1b23a37a91b2
Rishi Malik <rishimalik@light.home>
2012-10-14 11:20:36 -0400

    Url defs

--- coffee/settings.py
+++ coffee/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'alert'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -149,3 +150,6 @@ LOGGING = {
         },
     }
 }
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
--- coffee/urls.py
+++ coffee/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^coffee/$','coffee.views.index'),
+    url(r'^coffee/register','coffee.views.register')
 )
