0de3979748a09b599bb2b65210651ed9da9e7af5
Rishi Malik <rishimalik@light.home>
2012-10-18 08:19:02 -0400

    Pacman installed python3

--- coffeeclient.py
+++ coffeeclient.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import traceback
 import subprocess
 
@@ -8,20 +8,20 @@ SECRET = "233499"
 URL = "http://guarded-reef-2427.herokuapp.com/coffee/long/" + SECRET
 
 def alarm():
-    subprocess.call("./alarm.sh")
+    subprocess.call("/etc/coffee/alarm.sh")
 
 while 1:
     try:
-        print URL
-        resp = urllib2.urlopen(URL)
+        print(URL)
+        resp = urllib.request.urlopen(URL)
         data = resp.read()
-        print data
-        if (data.find("COFFEE") > -1):
+        print(data)
+        if (data.find(b"COFFEE") > -1):
             alarm()
     except KeyboardInterrupt:
         break
     except:
-        print "Exception, retrying: "
+        print("Exception, retrying: ")
         traceback.print_exc()
 
 
